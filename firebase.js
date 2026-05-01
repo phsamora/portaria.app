ccc5e4946cf687d2f3ab346685f760d389fc0cfb
phsamora <phsamorazes@gmail.com>
2026-05-01 01:52:04 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -40,12 +40,13 @@ import {
 
 /* -------- 🔧 SUBSTITUA AQUI COM SUAS CREDENCIAIS -------- */
 const firebaseConfig = {
-  apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  authDomain: "seu-projeto.firebaseapp.com",
-  projectId: "seu-projeto-id",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdefghijklmno"
+  apiKey: "AIzaSyBMi8I-t4tBrD71gpFkPPra9yuRLmVXCo0",
+  authDomain: "portaria-condominio-2a03f.firebaseapp.com",
+  databaseURL: "https://portaria-condominio-2a03f-default-rtdb.firebaseio.com",
+  projectId: "portaria-condominio-2a03f",
+  storageBucket: "portaria-condominio-2a03f.firebasestorage.app",
+  messagingSenderId: "865427708236",
+  appId: "1:865427708236:web:00281ab28926a48b00d7e9"
 };
 /* --------------------------------------------------------- */
 
